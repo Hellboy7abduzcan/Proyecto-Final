86bfe59da98f75f7dc23341756aea4c7e2a96bac
Hellboy7abduzcan <66399040+Hellboy7abduzcan@users.noreply.github.com>
2020-06-07 18:55:45 -0600

    Nodo (prueba)
    Este es un ejemplo, por favor, no lo tome en cuenta

---
+++ Nodo.java
@@ -0,0 +1,11 @@
+package gt.edu.umg.Progra1.Ejemplo;
+
+public class Nodo {
+	int elemento;
+	Nodo siguiente;
+
+	public Nodo(int elemento) {
+		this.elemento = elemento;
+		siguiente = null;
+	}
+}
